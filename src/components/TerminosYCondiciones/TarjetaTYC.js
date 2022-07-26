3734c324ac4b63bfd9a54feb85fb80a4f46daa2f
SammyBM <62446680+SammyBM@users.noreply.github.com>
2022-07-26 01:59:40 -0500

    DialogActions imported

--- src/components/TerminosYCondiciones/TarjetaTYC.js
+++ src/components/TerminosYCondiciones/TarjetaTYC.js
@@ -1,4 +1,4 @@
-import { Button, Dialog, DialogContent, DialogTitle } from '@mui/material';
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from '@mui/material';
 import * as React from 'react';
 import { useState } from 'react';
 
