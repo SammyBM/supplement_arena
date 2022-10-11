d2521c963571630ec071f2a233eb86e619780003
SammyBM <62446680+SammyBM@users.noreply.github.com>
2022-10-11 12:30:49 -0500

    Panel de usuarios, tipo de usuarios c/persistencia

--- src/components/MenuAdmin/PanelUsuarios.js
+++ src/components/MenuAdmin/PanelUsuarios.js
@@ -1,11 +1,36 @@
-import { Table, TableBody, TableCell, TableHead, TableRow } from '@mui/material';
+import { Container, Fab, Table, TableBody, TableCell, TableHead, TableRow } from '@mui/material';
+import SaveIcon from '@mui/icons-material/Save';
 import * as React from 'react';
 import Service from '../../Service';
 import SinResultados from '../Errores/SinResultados';
+import TipoUsuario from './TipoUsuario';
 
 function PanelUsuarios() {
     const [usuarios, setUsuarios] = React.useState([]);
+    const [hasChanges, setHasChanges] = React.useState(false);
+    const [forceChange, setForceChange] = React.useState(false);
+
+    const MAX_WIDTH = "md";
+    const FAB_STYLE = {
+        display: { xs: hasChanges ? "block" : "none" },
+        position: 'sticky',
+        bottom: 20,
+        right: 20,
+    };
     var columnas = [{ label: "Nombre Usuario" }, { label: "Nombre Completo" }, { label: "Email" }, { label: "Tipo Usuario" }];
+
+    const handleChildrenState = () => {
+        console.log(hasChanges);
+        for (let usr of usuarios) {
+            if (hasChanges) {
+                setForceChange(true);
+            }
+            else
+                setForceChange(false);
+
+        }
+        setHasChanges(false);
+    }
 
     React.useEffect(() => {
         Service.getData('usuarios/read').then((data) => {
@@ -23,26 +48,32 @@ function PanelUsuarios() {
 
     if (usuarios.length > 0) {
         return (
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        {columnas.map((columna) => <TableCell key={columna.id} padding={columna.disablePadding ? 'none' : 'normal'}>{columna.label}</TableCell>)}
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {
-                        usuarios.map((usuario) =>
+            <>
+                <Container maxWidth={MAX_WIDTH}>
+                    <Table>
+                        <TableHead>
                             <TableRow>
-                                <TableCell>{usuario.nombreUsuario}</TableCell>
-                                <TableCell>{usuario.nombre + ' ' + usuario.apellido}</TableCell>
-                                <TableCell>{usuario.correo}</TableCell>
-                                <TableCell>{usuario.tipoUsuarioID}</TableCell>
-                                {/* <TableCell><TipoUsuario usuario={usuario}/></TableCell> */}
+                                {columnas.map((columna) => <TableCell key={columna.id} padding={columna.disablePadding ? 'none' : 'normal'}>{columna.label}</TableCell>)}
                             </TableRow>
-                        )
-                    }
-                </TableBody>
-            </Table>
+                        </TableHead>
+                        <TableBody>
+                            {
+                                usuarios.map((usuario) =>
+                                    <TableRow>
+                                        <TableCell>{usuario.nombreUsuario}</TableCell>
+                                        <TableCell>{usuario.nombre + ' ' + usuario.apellido}</TableCell>
+                                        <TableCell>{usuario.correo}</TableCell>
+                                        <TableCell><TipoUsuario usuario={usuario} toSave={setHasChanges} forceChange={forceChange} /></TableCell>
+                                    </TableRow>
+                                )
+                            }
+                        </TableBody>
+                    </Table>
+                </Container>
+                <Fab sx={FAB_STYLE} color="primary" aria-label='guardar' onClick={handleChildrenState}>
+                    <SaveIcon />
+                </Fab>
+            </>
 
         );
     }
--- src/components/MenuAdmin/TipoUsuario.js
+++ src/components/MenuAdmin/TipoUsuario.js
@@ -1,50 +1,103 @@
+import { CircularProgress, FormControl, FormHelperText, InputLabel, MenuItem, Select, StyledEngineProvider } from '@mui/material';
 import * as React from 'react';
 import Service from '../../Service';
 
 function TipoUsuario(props) {
     const [tipo, setTipo] = React.useState(props.usuario.tipoUsuarioID);
-    const tiposUsuario = [];
+    const [comp, setComp] = React.useState(3);
+    const [userTypes, setUserTypes] = React.useState([]);
+    const [hasChanged, setHasChanged] = React.useState(false);
+
+    const findTipo = (tipo) => {
+        return tipo.tipoUsuarioID == props.usuario.tipoUsuarioID;
+    }
+
+    const changeValue = (value) => {
+        for (let type of userTypes) {
+            if (value === type.tipo)
+                return type;
+        }
+    }
 
     const handleChange = (e) => {
-        let prevType = tipo;
-        setTipo(e.target.value);
+        setTipo(changeValue(e.target.value));
+        setHasChanged(true);
+        props.toSave(true);
+        return;
+    }
 
-        //HTTP PUT tipoUsuarioID -> props.usuario.usuarioID
-        Service.postData("usuarios/update",
-            {
-                ...props.usuario,
-                tipoUsuarioID: tipo
-            }
-        ).then((resp) => {
-            console.log("Se logró!!");
-            //Notificar usuario
-        }).catch((err) => {
-            console.error("Error: " + err);
-            setTipo(prevType);
-            //Notificar usuario
-
-        });
+    const persistData = () => {
+        if (!hasChanged)
+            return;
+        else
+            //HTTP PUT tipoUsuarioID -> props.usuario.usuarioID
+            Service.postData("usuarios/update",
+                {
+                    ...props.usuario,
+                    tipoUsuarioID: tipo.tipoUsuarioID
+                }
+            ).then((resp) => {
+                console.log(resp);
+                //Notificar usuario
+            }).catch((err) => {
+                console.error("Error: " + err);
+                //Notificar usuario
+            });
+        setHasChanged(false);
+        return;
     }
 
     React.useEffect(() => {
         //HTTP GET tipoUsuarioID & tipoUsuario.tipo
-        //Falta esta parte de API
-    }, []);
+        if (props.forceChange)
+            persistData();
+        else
+            setTimeout(async () => {
+                await Service.getData('tipos_usuario/read').then((res) => {
+                    setUserTypes(res.records);
+                    setTipo(res.records.find(findTipo));
+                }).then(() => setComp(1)).catch((err) => {
+                    console.error(err);
+                    setComp(2);
+                })
+                if (userTypes.length > 0)
+                    setTipo(userTypes.find(findTipo));
+            }, 3000)
 
-    return (
-        <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-            <InputLabel id="selector-tipo">Tipo usuario</InputLabel>
-            <Select
-                labelId="selector-tipo"
-                id="selector-tipo"
-                value={tipo}
-                label="Tipo usuario"
-                onChange={handleChange}
-            >
-                {tiposUsuario.map(tipo => <MenuItem value={tipo.id}>{tipo.valor}</MenuItem>)}
-            </Select>
-        </FormControl>
-    )
+    }, [props.forceChange]);
+
+    switch (comp) {
+        case 1:
+            return (<FormControl sx={{ m: 1, minWidth: 90 }} size="small">
+                <InputLabel id="selector-tipo">Tipo usuario</InputLabel>
+                <Select
+                    labelId="selector-tipo"
+                    id="selector-tipo"
+                    value={tipo !== undefined ? tipo.tipo : props.usuario.tipoUsuarioID}
+                    label="Tipo usuario"
+                    onChange={handleChange}
+                >
+                    {userTypes.map(tipo => <MenuItem key={tipo.tipoUsuarioID} value={tipo.tipo}>{tipo.tipo}</MenuItem>)}
+                </Select>
+            </FormControl>)
+        case 2:
+            return (<FormControl sx={{ m: 1, minWidth: 90 }} size="small" disabled>
+                <InputLabel id="error">Tipo usuario</InputLabel>
+                <Select
+                    labelId="error"
+                    id="error"
+                    value={props.usuario.tipoUsuarioID}
+                    label="Tipo usuario"
+                    onChange={handleChange}
+                />
+                <FormHelperText>Error al cargar</FormHelperText>
+            </FormControl>)
+        case 3:
+            return <CircularProgress />
+        default:
+            return null;
+
+    }
 }
 
 export default TipoUsuario;
