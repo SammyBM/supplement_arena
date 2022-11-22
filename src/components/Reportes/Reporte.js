1f31355e14e0842d22559ab9f83a73e05f11b98f
SammyBM <62446680+SammyBM@users.noreply.github.com>
2022-11-22 07:26:20 -0600

    Getting better reports

--- src/components/Reportes/Reporte.js
+++ src/components/Reportes/Reporte.js
@@ -1,19 +1,22 @@
 import * as React from 'react';
-import { Button, Card, CardActionArea, CardActions, CardContent, CardHeader, IconButton, Stack, Typography } from '@mui/material';
+import { Button, Card, CardMedia, CardActions, CardContent, CardHeader, IconButton, Stack, Typography } from '@mui/material';
 import CheckIcon from "@mui/icons-material/Check"
+import { ROUTES } from "../../constantes";
+import axios from 'axios';
+
 
 export default function Reporte(props) {
     const [reporte, setReporte] = React.useState(
         {
-            Usuario: props.reporte.Usuario,
-            Fecha: props.reporte.Fecha,
-            Articulo: props.reporte.Articulo,
-            Resumen: props.reporte.Resumen,
-            Contenido: props.reporte.Contenido,
+            Usuario: props.reporte.usuarioID,
+            Fecha: props.reporte.fechaCreacion,
+            Articulo: props.reporte.articuloID,
+            Resumen: props.reporte.resumen,
+            Contenido: props.reporte.texto,
             Resuelto: false
         }
     );
-
+    const [loading, setLoading] = React.useState(true);
 
     const setResuelto = () => {
         setReporte({ ...reporte, Resuelto: true });
@@ -23,9 +26,24 @@ export default function Reporte(props) {
         console.log("abrir articulo");
     };
 
+    React.useEffect(() => {
+        setTimeout(async () => {
+            await axios.get(ROUTES.API_ROUTE + "/articulos/read_row.php?id=", props.reporte.articuloID).then((resp) => {
+                setReporte({
+                    ...reporte,
+                    Articulo: resp
+                });
+            }).catch((err) => {
+                console.error(err);
+            });
+        }, 1300)
+    }, []);
+
+
     return (
         <Card>
             <CardHeader title={reporte.Resumen} subheader={reporte.Fecha} />
+            {/* <CardMedia component="img" height="140" alt={reporte.Articulo.titulo} src={ROUTES.API_ROUTE + "/imagenes/imagenes_articulos/" + reporte.Articulo.imagen} /> */}
             <CardContent>
                 <Typography variant="body1" color="secondary">{reporte.Contenido}</Typography>
             </CardContent>
